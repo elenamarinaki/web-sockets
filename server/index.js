338e74f63d2a50f183102cd853dcb5f6231dcbe7
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-11-28 23:15:37 +0000

    message handler function

--- server/index.js
+++ server/index.js
@@ -4,6 +4,21 @@ const path = require('path');
 
 const app = express();
 
+app.use('/', express.static(path.resolve(__dirname, '../client')));
+
+// regular express http server
+const myServer = app.listen(9876);
+
+// the websocket server
 const wsServer = new WebSocket.Server({
   noServer: true,
 });
+
+// what the server will do on message event
+const messageHandler = (msg) => {
+  wsServer.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(msg.toString());
+    }
+  });
+};
