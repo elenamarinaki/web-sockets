4589b01b767f2ec49917e681c8f8b9ee00aafe55
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-11-29 00:33:03 +0000

    upgrading HTTP to websocket

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use('/', express.static(path.resolve(__dirname, '../client')));
 
+// ------------------------ CREATING THE SERVERS
+// -------------------------------------------------------------
+
 // regular express http server
 const myServer = app.listen(9876);
 
@@ -13,6 +16,22 @@ const myServer = app.listen(9876);
 const wsServer = new WebSocket.Server({
   noServer: true,
 });
+
+// ------------------------ upgrading from HTTP to websocket
+const upgrade = (request, socket, head) => {
+  // is connection going to be accepted?
+  if (Math.random() > 0.5) {
+    return socket.end('HTTP/1.1 401 Unauthorized\r\n', 'ascii');
+  }
+  // if yes, ...
+  wsServer.handleUpgrade(request, socket, head, (ws) => {
+    wsServer.emit('connection', ws, request);
+  });
+};
+
+// ----------------------------------- handle event
+myServer.on('upgrade', upgrade);
+
 
 // what the server will do on message event
 const messageHandler = (msg) => {
